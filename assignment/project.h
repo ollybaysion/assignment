7717b97585432418ff575b23f345f56a2181df25
K <ollybaysion@naver.com>
2018-12-03 16:25:10 +0900

    add hashtable.h.
    I don't know how to implement hashtable.
    Node -> hashTable (Node **table) (second dimensional array hashtable)
    or Node -> List -> hashTable (List *table) (oneth array hashtable)
    
    There is good and bad for each choice.

--- assignment/project.h
+++ assignment/project.h
@@ -1,28 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// body for store data
-typedef struct _body {
-	char *key;
-	int value;
-} Body;
-
-// node for hash table
-typedef struct _node {
-	struct _node *next;
-	Body data;
-} Node;
-
-// I : hash table
-typedef  struct _hashTable {
-
-} HashTable;
-
-typedef HashTable LIST;
-
 // functions for hash table
-void HTinit(List *e);
-void putHT(LIST *e, char *key, int value);
+void HTinit(LIST *e, HashFunc *f);
+void putHT(LIST *e, char *lkey, int lvalue);
 Body getHT(LIST *e, char *lkey);
 int deleteHT(LIST *e, char *lkey, Body *dataPos);
 int countHT(LIST *e);
