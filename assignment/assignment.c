9d13657695a87493ebe1b8eb3c70e8f1143794ef
ollybaysion <ollybaysion@naver.com>
2018-12-11 22:44:30 +0900

    Update assignment.c

--- assignment/assignment.c
+++ assignment/assignment.c
@@ -63,18 +63,7 @@ int hashString(char *str);
 Body *searchHT(TABLE *e, char *find);
 
 // FREE : functions for implement each functions above.
-int isAlpha(char ch);
-int isNum(char ch);
-int numToString(char *des, int src);
-
-void addRLE(char *des, char ch, int len);
-void pullRLE(char *des, char *src, int first, int last);
-int searchRLE(char *src, char *cmp);
-void deleteRLE(char *src, int first, int last);
-void insertRLE(char *src, char *str, int idx);
-int RLEinfo(char *src, char *dch, int *dlen);
-int RLElen(char *src);
-void optimizeRLE(char *src);
+void decompressRLE(char *des, char *src);
 
 // for hash List in hash table.
 void ListInit(LIST *k)
@@ -149,7 +138,7 @@ void putHT(TABLE *e, Body *bd)
 			curAlpha = curAlpha->next_alpha;
 			pre = pre->next_alpha;
 		}
-		
+
 		newNode->next_alpha = pre->next_alpha;
 		pre->next_alpha = newNode;
 	}
@@ -209,6 +198,28 @@ int deleteHT(TABLE *e, char *lkey)
 	return 0;
 }
 
+Body *searchHT(TABLE *e, char *find)
+{
+	int i = 0;
+	LIST *pos = &(e->table[0]);
+	for (i = 0; i < 1000; i++)
+	{
+		pos = &(e->table[i]);
+		Node *search = pos->head->next;
+		while (1)
+		{
+			if (powerCompare(search->data.key, find))
+			{
+				return &(search->data);
+			}
+			if (search->next == NULL) break;
+			search = search->next;
+		}
+	}
+
+	return NULL;
+}
+
 int hashString(char *str)
 {
 	int sum = 0;
@@ -228,181 +239,44 @@ int isNum(char ch)
 	return 0;
 }
 
-int isAlpha(char ch)
-{
-	if (ch >= 'a' && ch <= 'z') return 1;
-	if (ch >= 'A' && ch <= 'Z') return 1;
-	return 0;
-}
 
-int numToString(char *des, int src)
+void decompressRLE(char *des, char *src)
 {
-	int d[10];
-	int len;
-	for (len = 0; src; len++)
-	{
-		d[len] = src % 10;
-		src /= 10;
-	}
-	for (int i = len; i > 0; i--)
-	{
-		*des++ = d[i - 1] + '0';
-	}
-	*des = '\0';
-	return len;
-}
-
-void addRLE(char *des, char ch, int len)
-{
-	char num[10];
-	char onechar[2] = { ch, '\0' };
-	numToString(num, len);
-	strcat(des, onechar);
-	strcat(des, num);
-}
-
-void pullRLE(char *des, char *src, int first, int last)
-{
-	char chs[MAX_WORD];
-	int lens[MAX_WORD];
-	int cnt = RLEinfo(src, chs, lens);
-
-	int idx = 0;
+	char *s = src;
+	char result[300] = "";
 	int num = 0;
-	int sum = lens[0];
+	char ch;
+	while (1)
+	{
+		ch = *s;
+		s++;
 
-	while (first > sum)
-	{
-		idx++;
-		sum += lens[idx];
-	}
-	for (int cur = first; cur <= last; cur++)
-	{
-		num++;
-		if (cur == sum || cur == last)
+		if (isNum(*s))
 		{
-			if (num) addRLE(des, chs[idx], num);
-			idx++;
-			sum += lens[idx];
-			num = 0;
-		}
-	}
-}
-
-int searchRLE(char *src, char *cmp)
-{
-	int len_src = RLElen(src);
-	int len_cmp = RLElen(cmp);
-	for (int i = 1; i <= len_src - len_cmp + 1; i++)
-	{
-		char tmp[300] = "";
-		pullRLE(tmp, src, i, i + len_cmp - 1);
-		if (!strcmp(cmp, tmp)) return i;
-	}
-
-	return 0;
-}
-
-void deleteRLE(char *src, int first, int last)
-{
-	char str1[100] = "";
-	char str2[100] = "";
-	char buf[300] = "";
-
-	pullRLE(str1, src, 1, first - 1);
-	pullRLE(str2, src, last + 1, RLElen(src));
-
-	strcat(buf, str1);
-	strcat(buf, str2);
-	strcpy(src, buf);
-
-	optimizeRLE(src);
-}
-
-void insertRLE(char *src, char *str, int idx)
-{
-	char str1[100] = "";
-	char str2[100] = "";
-	char buf[100] = "";
-
-	pullRLE(str1, src, 1, idx);
-	pullRLE(str2, src, idx + 1, RLElen(src));
-
-	strcat(buf, str1);
-	strcat(buf, str);
-	strcat(buf, str2);
-	strcpy(src, buf);
-	optimizeRLE(src);
-}
-
-int RLEinfo(char *src, char *dch, int *dlen)
-{
-	char *ch = dch; int *len = dlen;
-	while (*src)
-	{
-		*len = 0;
-		if (isAlpha(*src))
-		{
-			*ch = *src;
-			ch++; src++;
-		}
-		while (isNum(*src))
-		{
-			*len *= 10;
-			*len += *src++ - '0';
-			if (isAlpha(*src) || !(*src)) len++;
-		}
-	}
-	*ch = '\0';
-	return (ch - dch) / sizeof(char);
-}
-
-int RLElen(char *src)
-{
-	char ch[MAX_WORD];
-	int len[MAX_WORD];
-	int cnt = RLEinfo(src, ch, len);
-	int sum = 0;
-	for (int i = 0; i < cnt; i++) sum += len[i];
-	return sum;
-}
-
-void optimizeRLE(char *src)
-{
-	char ch[MAX_WORD];
-	int len[MAX_WORD];
-	int cnt = RLEinfo(src, ch, len);
-
-	char buf[300] = "";
-	char nch[10];
-	int nlen[10];
-	int j = 0;
-
-	nch[0] = ch[0];
-	nlen[0] = len[0];
-	for (int i = 1; i < cnt; i++)
-	{
-		if (ch[i] != ch[i - 1])
-		{
-			j++;
-			nch[j] = ch[i];
-			nlen[j] = len[i];
+			while (isNum(*s))
+			{
+				num *= 10;
+				num += *s - '0';
+				s++;
+			}
+			while (num)
+			{
+				num--;
+				char onech[2] = { ch, '\0' };
+				strcat(result, onech);
+			}
 		}
 		else
 		{
-			nlen[j] += len[i];
+			char onech[2] = { ch, '\0' };
+			strcat(result, onech);
 		}
+
+		if (*s == '\0') break;
 	}
 
-	for (int i = 0; i <= j; i++)
-	{
-		addRLE(buf, nch[i], nlen[i]);
-	}
-	strcpy(src, buf);
+	strcat(des, result);
 }
-
-// ab
-// ab
 
 int alphaCompare(char *src, char *str)
 {
@@ -410,8 +284,10 @@ int alphaCompare(char *src, char *str)
 	return 1;
 }
 
-int powerCompare(char *src, char *str)
+int powerCompare(char *src, char *com)
 {
+	char str[100] = "";
+	decompressRLE(str, com);
 	char *s = str;
 	char *d = src;
 	int is = 0;
@@ -487,6 +363,17 @@ void input(TABLE *e, char op, Body *data, FILE *outputFile, char *fileName)
 		}
 		printf("%s\n", fileName);
 		break;
+	case 'S':
+		sPos = searchHT(e, data->key);
+		if (sPos != NULL)
+		{
+			printf("%d\n", sPos->value);
+		}
+		else
+		{
+			printf("NONE\n");
+		}
+		break;
 	}
 }
 
@@ -496,9 +383,9 @@ int main(int argc, char *argv[])
 	FILE *outputFile;
 	inputFile = fopen(argv[1], "r");
 	char buf[300] = "";
-	int data = 0; 
+	int data = 0;
 	char FileName[300] = "";
-	Body inputData; 
+	Body inputData;
 
 	TABLE myHT;
 	HTinit(&myHT, hashString, alphaCompare);
@@ -543,6 +430,5 @@ int main(int argc, char *argv[])
 
 		input(&myHT, op, &inputData, outputFile, FileName);
 	}
-
 	return 0;
 }
